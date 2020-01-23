cb2a228f489287ede24cce63da61784b3bbcc9a8
Osemudiamen <55854888+Ose-Me@users.noreply.github.com>
2020-01-23 17:29:48 +0100

    Update repeat string.js

--- repeat string.js
+++ repeat string.js
@@ -1,14 +1,12 @@
 function repeatStringNumTimes(str, num) {
-    var string 
-    // while (num > 0){
-    //   string = str.concat(string);
-    //   num--;
-    // }
-    for (var i = 1;i <= num;i++){
-    string = str.concat(str);
-    }
-    return string;
-    console.log(string);
+  var string = "";
+  while (num > 0){
+    string = string.concat(str);
+    num--;
   }
-  repeatStringNumTimes("abc", 3);
+  return string;
+  console.log(string);
+}
+repeatStringNumTimes("abc", 3);
+
   

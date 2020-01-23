2a0bda7c76e8f34917947f49427179dfe58dd3d2
Osemudiamen <55854888+Ose-Me@users.noreply.github.com>
2020-01-23 13:12:52 +0100

    Add files via upload

---
+++ reverse string.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+function findLongestWordLength(str) {
+    var longest = 0;
+    var result = str.split(" ");
+    for (var i= 0; i< result.length ;i++){
+      if(result[i].length > longest){
+        longest = result[i].length;
+      }
+    }
+    return longest;
+  
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
+  

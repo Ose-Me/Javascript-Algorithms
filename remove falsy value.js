4a0eab702d2d1a2c4e4b7d075d7ad8303a91d486
Osemudiamen <55854888+Ose-Me@users.noreply.github.com>
2020-01-28 10:38:26 +0100

    Update remove falsy value.js

--- remove falsy value.js
+++ remove falsy value.js
@@ -1,11 +1,8 @@
 function bouncer(arr) {
-    var newArr= [];
+    let newArr= [];
    for(var i = 0;i< arr.length;i++){
-     if(!!arr[i]===true){
-     newArr.push(arr[i])}
-       }console.log(newArr);
+      if (arr[i]) newArr.push(arr[i]);
+  } console.log(newArr);
    return newArr;
  }
- 
- bouncer([7, "ate", "", false, 9]);
- 
+ bouncer([7, "ate", "", false, 9]); 

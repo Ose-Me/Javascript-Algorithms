49d5047fe0d312a6566e41f07d98db546220e030
Osemudiamen <55854888+Ose-Me@users.noreply.github.com>
2020-02-06 12:47:40 +0100

    Update wherefor art thou.js

--- wherefor art thou.js
+++ wherefor art thou.js
@@ -1,18 +1,20 @@
 function whatIsInAName(collection, source) {
-    // What's in a name?
-    var arr = [];
-    // Only change code below this line
-  for(var i = 0;i <collection.length; i++){
-    if(collection[i].last === source.last){
-     arr.push(collection[i])
-    }
-  }
-  var obj = { first: 'someVal' };
-  console.log(obj[Object.keys(obj)[0]]);
-  console.log(collection[0][Object.keys(collection)[2]])
-    // Only change code above this line
-    return arr;
-  }
-  
-  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
-  
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  var keys = Object.keys(source);
+  arr = collection.filter((item) => {
+    var pass = true;
+    keys.map(key => {
+      if (item[key] !== source[key]) {
+        pass = false;
+      }
+    })
+    return pass;
+  })
+
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", la3: "Capulet" }], { last: "Capulet", fgh:"ghj"});
